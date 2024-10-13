3f4f49c23da76dae6da0ee406e924a769b19d90f
Lamnn16 <nhatlamnguyen.vn@gmail.com>
2024-10-13 13:02:18 +0200

    Initialize project
    Signed-off-by: Lamnn16 <nhatlamnguyen.vn@gmail.com>

---
+++ VietOS_Keil/Objects/main.d
@@ -0,0 +1,4 @@
+./objects/main.o: main.c led.h \
+  C:\Program\ Files\ARM\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
+  C:\Program\ Files\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
+  C:\Program\ Files\ARM\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
---
+++ VietOS_Keil/led.c
@@ -0,0 +1,24 @@
+#include "led.h"
+
+#define GPIOA_EN 		(1U<<0)
+#define LED_PIN 		(1U<<5)
+
+void led_init(void)
+{
+	/* Enable clock access to led port - PORTA */
+	RCC->AHB1ENR |= GPIOA_EN;
+	
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &= ~(1U<<11);
+}
+
+void led_on(void)
+{
+  /* Set PA5 */
+	GPIOA->BSRR  |= LED_PIN;
+}
+
+void led_off(void){
+	GPIOA->BSRR  &= ~LED_PIN;
+}
+	
