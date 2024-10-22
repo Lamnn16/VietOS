c9caaa2291ee38ba6f61df8ca68381b941e373c9
Lamnn16 <nhatlamnguyen.vn@gmail.com>
2024-10-22 16:24:24 +0200

    Add gitignore file
    Signed-off-by: Lamnn16 <nhatlamnguyen.vn@gmail.com>

--- VietOS_Keil/Objects/timebase.d
+++
@@ -1,4 +0,0 @@
-./objects/timebase.o: timebase.c timebase.h \
-  C:\Program\ Files\ARM\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\stm32f411xe.h \
-  C:\Program\ Files\ARM\Packs\ARM\CMSIS\6.1.0\CMSIS\Core\Include\core_cm4.h \
-  C:\Program\ Files\ARM\Packs\Keil\STM32F4xx_DFP\2.17.1\Drivers\CMSIS\Device\ST\STM32F4xx\Include\system_stm32f4xx.h
